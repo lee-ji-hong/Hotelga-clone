9cfc2d5b34805b2e989bb5758e2820ecd2d3b58a
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-05-25 11:36:24 +0900

    DELETE ALL

--- tests/unit/home.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import Home from '@/components/Home.vue'
-
-describe('HelloWorld.vue', () => {
-    const wrapper = shallowMount(Home);
-  it('renders Homepage', () => {
-    expect(wrapper.find('[data-test="main-page"]').exists()).toBe(true);
-  })
-})
